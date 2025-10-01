e45f088e69cc1673bd971f39103a5d4ecf764324
Thiago Zimermann Ferrari <thiagozimermannferraripy@gmail.com>
2025-10-01 03:48:02 +0000

    Super Trunfo Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,77 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int main(){
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    //Informações usuário
+    printf("Seja bem-vindo ao jogo Super Trunfo!\n");
+    printf("Jogo em desenvolvimento!\n");
+    printf("Cadastre suas carta para jogar!\n");
+    printf("Versão 1.0\n");
 
-  // Área para entrada de dados
+    //Variáveis
+    //Carta 1
+    char cidade_1[50];
+    int populacao_1, p_turistico_1;
+    float area_1;
+    double pib_1;
 
-  // Área para exibição dos dados da cidade
+    //Carta 2
+    char cidade_2[50];
+    int populacao_2, p_turistico_2;
+    float area_2;
+    double pib_2;
 
-return 0;
-} 
+    //Entrada de Dados
+    //Carta 1
+    printf("\nCidade: ");
+    scanf("%s", cidade_1);
+
+    printf("\nPopulação: ");
+    scanf("%d", &populacao_1);
+
+    printf("\nÁrea do município: ");
+    scanf("%f", &area_1);
+
+    printf("\nPIB do muncípio: ");
+    scanf("%f", &pib_1);
+
+    printf("\nPontos turisticos: ");
+    scanf("%d", &p_turistico_1);
+
+    //Carta 2
+    printf("Cidade: ");
+    scanf("%s", cidade_2);
+
+    printf("\nPopulação: ");
+    scanf("%d", &populacao_2);
+
+    printf("\nÁrea do município: ");
+    scanf("%f", &area_2);
+
+    printf("\nPIB do muncípio: ");
+    scanf("%f", &pib_2);
+
+    printf("\nPontos turisticos: ");
+    scanf("%d", &p_turistico_2);
+
+    //Saída de dados
+    //Carta 1
+    printf("\nCADASTRO DE CARTAS SUPER TRUNFO");
+    printf("\nCarta 1\n");
+    printf("\nCidade: %s.\n", cidade_1);
+    printf("\nPopulação: %d.\n", populacao_1);
+    printf("\nArea do Município: %.2f.\n", area_1);
+    printf("\nPIB do Município: %.2f.\n", pib_1);
+    printf("\nPontos Turísticos: %d.\n", p_turistico_1);
+
+    //Carta 2
+    printf("\nCarta 2\n");
+    printf("\nCidade: %s.\n", cidade_2);
+    printf("\nPopulação: %d.\n", populacao_2);
+    printf("\nArea do Município: %.2f.\n", area_2);
+    printf("\nPIB do Município: %.2f.\n", pib_2);
+    printf("\nPontos Turísticos: %d.\n", p_turistico_2);
+
+    return 0;
+
+}
